60a541bfa0922f0ddcd82b5af3544f03aaa5322a
Maxim Eremin <mderemin@gmail.com>
2019-10-19 13:22:06 +0300

    Exponent Smooth release

--- Python/Code/ExponentSmoothing/main.py
+++ Python/Code/ExponentSmoothing/main.py
@@ -1,9 +1,10 @@
-from exponentsmoothing import exponential_smoothing
 from math import floor, sqrt, isnan
 import decimal
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from exponentsmoothing import exponential_smoothing
+import time
 
 def flooring(x):
     result = 0
@@ -12,29 +13,61 @@ def flooring(x):
     return result
 
 def open_data(path):
-    X = np.array([])
-    data = np.fromfile(path)
-    a = 0
-    b = 200
-    y = []
-    for j in range(a, b):
-        y.append(0.0 - flooring(data[j]))
-    X.append([np.array(y)])
-    return np.array(X)
+    result = []
+
+    print(f'Data is loading.')
+
+    start = time.perf_counter()
+
+    result = np.fromfile(path)
+
+    end = time.perf_counter()
+
+    print(f'Data loaded.\tTime has passed:\t{end - start}')
+    return result
+
+def split_data(data, start = 0, end = 0):
+    Y = data[:end]
+    print(f'Data is starting to preparing.\tLength of data:\t{len(Y)}')
+
+    Y_result = []
+
+    start_t = time.perf_counter()
+
+    for dot in Y:
+        Y_result.append(-flooring(dot))
+
+    end_t = time.perf_counter()
+
+    X = range(start, end)
+    
+    print(f'Data is prepared.\tTime has passed:\t{end_t - start_t}\tLoaded dots:\t{len(Y)}')
+    return X, Y_result
 
 def main():
-    path = "./I01.dat"
-    dataset = open_data(path)
-    print(len(dataset))
-    with plt.style.context('seaborn-white'):    
-        plt.figure(figsize=(20, 8))
-        for alpha in [0.3, 0.05]:
-            plt.plot(exponential_smoothing(dataset.Users, alpha), label="Alpha {}".format(alpha))
-        plt.plot(dataset.Users.values, "c", label = "Actual")
-        plt.legend(loc="best")
-        plt.axis('tight')
-        plt.title("Exponential Smoothing")
-        plt.grid(True)
+    print('Start')
 
+    path = "./I01.dat"
+
+    start = 0
+    end = 200
+
+    dataset = open_data(path)
+
+    X, Y = split_data(dataset, start=start, end=end)
+
+    before = plt.figure()
+    plt.plot(X, Y)
+    before.savefig('before.png')
+
+    Y_prepared = exponential_smoothing(Y, 0.3)
+
+    after = plt.figure()
+    plt.plot(X, Y_prepared)
+    after.savefig('after.png')
+
+    print('End')
+    return 0
+    
 if __name__ == "__main__":
     main()
