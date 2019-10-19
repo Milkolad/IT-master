5d110f70f3cabb84b7f57b9587d5c0d49875bd85
Maxim Eremin <mderemin@gmail.com>
2019-10-19 15:20:57 +0300

    Commit for review

--- Python/Code/Example_3_shelling/main.py
+++ Python/Code/Example_3_shelling/main.py
@@ -3,20 +3,14 @@ import random
 def happy(arr, ii, jj, counter, zhach):
     for count in range(ii - 1, ii + 2):
         for count1 in range(jj - 1, jj + 2):
-            # print("u", arr[count][count1])
             if arr[count][count1] == zhach:
-                counter = counter + 1 
-                # print("arr", arr[count][count1])
-	            # print(counter) 
+                counter = counter + 1
     if counter > 2:
-        # print("happy")
         return 1
     else:
-        # print("not happy")
         return 0
 
-def swap(ar):
-    n = len(ar)
+def swap(ar, n):
     for row in ar:
         print(' '.join([str(elem) for elem in row]))
         
@@ -25,29 +19,20 @@ def swap(ar):
     h = random.randint(1, n - 2)
     while (ar[g][h] == 0) or (happy(ar, g, h, counter, ar[g][h]) == 1):
         counter = 0
-        # print("is happy", g, h, ar[g][h])
         g = random.randint(1, n - 2)
         h = random.randint(1, n - 2)
     else:
-        # print("not happy is", g, h, ar[g][h])
         swapznach = ar[g][h]
         swapg = g
         swaph = h
-        # print("swap", swapznach, swapg, swaph)
 	
     while (ar[g][h] != 0 or happy(ar, g, h, counter, swapznach) != 1):
-        #print("yx", g, h)
         g = random.randint(1, n - 2)
         h = random.randint(1, n - 2)
-        #print("no swap")
     else:
-        #print("swap ar  ", ar[g][h], g, h)
         swap = ar[g][h]
         ar[g][h] = swapznach
         ar[swapg][swaph] = swap
-        # print("swap", swap)
-        # for row in ar:
-            # print(' '.join([str(elem) for elem in row]))
 
 def main():
     print("enter N")
@@ -64,7 +49,7 @@ def main():
     while 1:
         cc += 1
         print(cc)
-        swap(a)
+        swap(a, n)
 
 if __name__ == "__main__":
     main()
