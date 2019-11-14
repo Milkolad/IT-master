da7c1c8f87a349d633ee140e0b53f6c45474aa82
Maxim Eremin <mderemin@gmail.com>
2019-11-14 10:46:09 +0300

    New Experiment in Master Work

--- MasterWork/MLVision/ViolaJohnes/main.py
+++ MasterWork/MLVision/ViolaJohnes/main.py
@@ -1,11 +1,19 @@
 import numpy as np
 import os
 from PIL import Image
-from cv2 import CascadeClassifier
+from argparse import ArgumentParser
 
-def main():
-    classifier = CascadeClassifier()
+def integral_image(img):
+    new_img = np.array(img.shape)
+    
+
+def main(path):
     return 0
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser(description='Process path to file')
+    parser.add_argument('filepath', metavar='PATH', help='path for the file', type=str)
+    args = parser.parse_args()
+    print('\n')
+    main(args.filepath)
+    print('\n')
